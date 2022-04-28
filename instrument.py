a3ffd6e15bb03b9d6a581595f4e9b7954ee0492f
bloodspork <bloodspork@tutanota.com>
2022-04-27 20:23:25 -0700

    include average; formatting enhancements

--- instrument.py
+++ instrument.py
@@ -1,39 +1,44 @@
 # author: bloodspork
 # date  : 20220427
 
+from audioop import avg
 import time
 import functools
 import logging
 
 _func_timing_dict = {}
 
-def _collect(funcName, duration):
-    global func_timing_dict
-    if funcName not in func_timing_dict:
-        func_timing_dict[funcName] = duration
-    else:
-        _func_timing_dict[funcName] += duration
+def get_func_timing_dict():
+    global _func_timing_dict
+    return _func_timing_dict
 
+def _collect(funcName, duration):
+    global _func_timing_dict
+    if funcName not in _func_timing_dict:
+        _func_timing_dict[funcName] = []
+    _func_timing_dict[funcName].append(duration)
 
 def timeit(func):
     @functools.wraps(func)
-    def generic_wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         ret = func(*args, **kwargs)
         end_time = time.perf_counter()
         _collect(f"{func.__name__!r}", end_time - start_time)
         return ret
+    return wrapper
 
-
-def getDurationsSummary():
-    return _func_timing_dict
-
-
-def logDurationsSummary(loggingLevel = logging.DEBUG):
-    ftd = getDurationsSummary()
-    ret = "Durations Summary\n"
-    for k,v in ftd:
-        ret += f"{k}: {v}\n"
-    logging.log(loggingLevel, ret)
+def log_duration_summary(loggingLevel = logging.DEBUG):
+    ftd = get_func_timing_dict()
+    msg = "Durations Summary\n"
+    for funcName, duration_list in ftd.items():
+        tot_time = sum(duration_list)
+        num_calls = len(duration_list)
+        avg_time = tot_time / num_calls
+        msg += funcName.rjust(30, ' ')
+        msg += f"  #calls: {str(num_calls).rjust(10)}" \
+               f"  avg: {str(round(avg_time, 3)).rjust(10, ' ')}" \
+               f"  tot: {str(round(tot_time, 3)).rjust(10, ' ')}\n"             
+    logging.log(loggingLevel, msg)
 
 
---
+++ test.py
@@ -0,0 +1,30 @@
+import time
+import logging
+
+from instrument import timeit, log_duration_summary
+
+@timeit
+def test_func_sleep_1():
+    time.sleep(1)
+
+@timeit
+def test_func_sleep_3():
+    time.sleep(3)
+
+def setup_logging():
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger('')
+    logger.setLevel(logging.DEBUG)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(formatter)
+    logger.addHandler(consoleHandler)
+
+if __name__ == "__main__":
+    setup_logging()
+
+    test_func_sleep_3()
+    for i in range(7):
+        test_func_sleep_1()
+
+    log_duration_summary()
